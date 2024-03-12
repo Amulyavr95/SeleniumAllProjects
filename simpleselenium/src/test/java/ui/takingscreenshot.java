d16c3cccffd205eb8c5b3b825740e065ca6050b4
Amulya V R <avr@BLR.ALLEGISINDIA.COM>
2024-03-12 12:53:12 +0530

    Initial Commit

---
+++ simpleselenium/src/test/java/ui/takingscreenshot.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class takingscreenshot {
+
+	public static void main(String[] args) throws IOException {
+		 WebDriverManager.chromedriver().setup();
+			ChromeDriver driver=new ChromeDriver();
+			driver.get("http://www.ebay.com");
+			driver.manage().window().maximize();
+			File f=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+             Files.copy(f, new File("C:\\Users\\avr\\Documents\\ss\\image.jpg"));
+	}
+
+}
